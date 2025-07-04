6080ffa9a2770765395349b1c3f2ce9af73ab845
Potturu-Kavya <kavyapotturu@gmail.com>
2025-07-04 15:47:13 +0530

    Java Fundamentals

---
+++ Java_Fundamentals/src/Scanner/Question1.java
@@ -0,0 +1,21 @@
+package Scanner;
+
+//import java.util.Scanner;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				Scanner obj=new Scanner(System.in);
+				String a=obj.nextLine();
+				String b=obj.nextLine();
+				System.out.println(a+" Technologies "+b);
+			
+
+		
+
+
+	}
+
+}
