8aa9c63304f184ca266c94335224d1902be084a2
Potturu-Kavya <kavyapotturu@gmail.com>
2025-07-04 16:12:23 +0530

    Java Fundamentals

--- Java_Fundamentals/src/Question1.java
+++ Java_Fundamentals/src/Question1.java
@@ -1,11 +1,19 @@
 import java.util.Scanner;
 
 public class Question1 {
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String a=sc.nextLine();
-		String b=sc.nextLine();
-		System.out.println(a+" Technologies "+b);
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the two strings");
+		String a=scanner.nextLine();
+		String b=scanner.nextLine();
+		System.out.println(a+ " Technologies " +b);
+		
+		
+		
+
 	}
 
 }
+
